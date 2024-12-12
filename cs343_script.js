f792256eee956f8196fab6a1dab8bc77fb826d9f
samculp <samculp04@yahoo.com>
2024-12-11 22:57:58 -0600

    commented code, changed file names

--- cs343_script.js
+++ cs343_script.js
@@ -1,8 +1,10 @@
+// bind various HTML elements to variables
 const submitButton = document.getElementById("submit");
 const refreshButton = document.getElementById("refresh");
 const form = document.getElementById("quizForm");
 const resultText = document.getElementById("result");
 
+// object storing correct answers
 const correctAnswers= {
   q1: "Gold",
   q2: "Mauna Kea",
@@ -16,7 +18,9 @@ const correctAnswers= {
   q10: "Vatican City"
 }
 
+// event handler for submit button
 submitButton.addEventListener("click", () => {
+  // get all of the answers from the form
   const userAnswers = {
     q1: form.q1.value,
     q2: form.q2.value,
@@ -29,10 +33,13 @@ submitButton.addEventListener("click", () => {
     q9: form.q9.value,
     q10: form.q10.value
   }
+
   let score = 0;
+  
   for (const question in correctAnswers) {
     const selectedOption = document.querySelector(`input[name=${question}]:checked`);
     if (selectedOption) {
+      // if the question was answered, check if it is correct and update score
       if (userAnswers[question] === correctAnswers[question]) {
         score++;
         selectedOption.parentElement.style.background = "#A5FD82";
@@ -42,21 +49,32 @@ submitButton.addEventListener("click", () => {
       }
     }
   }
+
+  // display results
   resultText.textContent = `You scored ${score} out of ${Object.keys(correctAnswers).length}.`;
   resultText.scrollIntoView({behavior: "smooth"});
   console.log("Score: ", score);
+
+  // update displays for the buttons
   refreshButton.style.display = "inline";
   submitButton.disabled = true;
 })
 
+// event handler for refresh button
 refreshButton.addEventListener("click", () => {
+  // reset the form
   form.reset();
   resultText.textContent = ``;
+
+  // reset all the labels to default styling
   const labels = document.querySelectorAll("label");
   labels.forEach((label) => {
     label.style.background = "none";
   })
+
+  // update displays for the buttons
   refreshButton.style.display = "none";
   submitButton.disabled = false;
+
   document.getElementById("title").scrollIntoView({behavior: "smooth"});
 })
